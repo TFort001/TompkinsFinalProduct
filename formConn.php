048f8ce66e6063d3ba6673cb8bfc486fcef01f0e
TFort001 <114301919+TFort001@users.noreply.github.com>
2024-03-01 23:34:54 -0600

    Update formConn.php

--- formConn.php
+++ formConn.php
@@ -1,20 +1,13 @@
 <?php
 function formConn()
 {
-    $host = "tcp:tompkinsdatabaseserver.database.windows.net,1433";
-    $username = "tompkins_admin";
-    $password = "OfAllTrades1!";
-    $db_name = "tompkinsdb";
-    //Initializes MySQLi
-    $conn = mysqli_init();
-
-    mysqli_ssl_set($conn, NULL, NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-
-    // Establish the connection
-    mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
-
-    //If connection failed, show the error
-    if (mysqli_connect_errno()) {
-        die('Failed to connect to MySQL: ' . mysqli_connect_error());
-    }
+    // PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:tompkinsofalltrades-server.database.windows.net,1433; Database = tompkinsdb", "tompkinsofalltrades-server-admin", "Capstone1!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 }
