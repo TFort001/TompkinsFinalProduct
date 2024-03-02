b1718bb841a479869871f5e990d61ff5cab0c43b
TFort001 <114301919+TFort001@users.noreply.github.com>
2024-03-01 23:40:37 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -40,7 +40,7 @@
     <div class = "container-fluid">
       <header class="flexbox">Give us feedback or ask questions</header>
         <p class="flexbox">Please use this page only for asking questions about services or feedback about Tompkins Fitness/ website use</p>
-      <form action = "" id="contactform"  method ="post">
+      <form action = "formConn.php" id="contactform"  method ="post">
      
           <label for ="name">Please enter your first and last name:</label>
           <input type ="text" id ="fullname" size ="50" maxlength="50" class="form-control">
